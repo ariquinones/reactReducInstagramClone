d0d126dd81c06fe61b69fe882eeb5f1c3e862212
ari.quinones <ari.quinones@remuv.io>
2017-01-21 23:31:40 -0600

    react redux instagram copy

---
+++ client/store.js
@@ -0,0 +1,30 @@
+import { createStore, compose} from 'redux';
+import { syncHistoryWithStore} from 'react-router-redux';
+import { browserHistory} from 'react-router';
+
+// import the root reducers
+import rootReducer from './reducers/index';
+
+import comments from './data/comments';
+import posts from './data/posts';
+
+// create an object for the default data
+const defaultState = {
+	posts,
+	comments
+};
+
+const enhancers = compose(
+	window.devToolsExtensions ? window.devToolsExtensions() : f => f 
+);
+
+export const store = createStore(rootReducer, defaultState);
+
+export const history = syncHistoryWithStore(browserHistory, store);
+
+if(module.hot) {
+	module.hot.accept('./reducers/', () => {
+		const nextRootReducer = require('./reducers/index').default;
+		store.replaceReducer(nextRootReducer);
+	})
+}
